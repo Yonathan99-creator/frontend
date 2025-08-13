7fb806415e7235dd5bd566ca95547a30abdd8c6b
Yonathan González Lara <yonathangonzalez321@gmail.com>
2025-08-12 18:08:00 -0600

    Fix diff markers in CalendarView.tsx

--- src/components/professional/calendar/CalendarView.tsx
+++ src/components/professional/calendar/CalendarView.tsx
@@ -1,7 +1,5 @@
-@@ .. @@
- import { useState } from "react";
- import { ChevronLeft, ChevronRight, Plus, Calendar as CalendarIcon, Grid3X3, Columns, Square, Filter, Search, Bell, Settings, Download, Share2, Star, TrendingUp, Users, Clock, Award, Zap, BarChart3, Sparkles, Crown, Trophy, Flame, Rocket, Medal } from "lucide-react";
--import { useApp } from "../../contexts/AppContext";
-+import { useApp } from "../../../contexts/AppContext";
- import MonthView from "./MonthView";
- import WeekView from "./WeekView";
+import { useState } from "react";
+import { ChevronLeft, ChevronRight, Plus, Calendar as CalendarIcon, Grid3X3, Columns, Square, Filter, Search, Bell, Settings, Download, Share2, Star, TrendingUp, Users, Clock, Award, Zap, BarChart3, Sparkles, Crown, Trophy, Flame, Rocket, Medal } from "lucide-react";
+import { useApp } from "../../../contexts/AppContext";
+import MonthView from "./MonthView";
+import WeekView from "./WeekView";
