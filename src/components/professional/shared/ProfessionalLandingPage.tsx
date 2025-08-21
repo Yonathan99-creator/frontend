34ab923b74a172fbf9ec77b749ae569bf28d17d3
Yonathan González Lara <yonathangonzalez321@gmail.com>
2025-08-20 22:12:25 -0600

    Fix ProfessionalLandingPage.tsx syntax error

--- src/components/professional/shared/ProfessionalLandingPage.tsx
+++ src/components/professional/shared/ProfessionalLandingPage.tsx
@@ -1,1 +1,22 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+import React from 'react';
+import ProfessionalNavbar from './ProfessionalNavbar';
+import ProfessionalHero from '../landing/ProfessionalHero';
+import ProfessionalModules from '../landing/ProfessionalModules';
+import ProfessionalCalendar from '../landing/ProfessionalCalendar';
+import ProfessionalReviews from '../landing/ProfessionalReviews';
+import ProfessionalFooter from './ProfessionalFooter';
+
+const ProfessionalLandingPage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <ProfessionalNavbar />
+      <ProfessionalHero />
+      <ProfessionalModules />
+      <ProfessionalCalendar />
+      <ProfessionalReviews />
+      <ProfessionalFooter />
+    </div>
+  );
+};
+
+export default ProfessionalLandingPage;
